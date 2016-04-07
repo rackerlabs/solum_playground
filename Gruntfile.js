b31299e9081a996b1ddefc60119262cb99efa33c
Vijendar Komalla <vijendar.komalla@RACKSPACE.COM>
2016-04-07 10:26:22 -0500

    listen on port 80

--- Gruntfile.js
+++ Gruntfile.js
@@ -107,7 +107,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9090,
+        port: 80,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: '0.0.0.0',
         livereload: 35729
